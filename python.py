cad6400e360c719e959e0f08dde078650364f96e
kiranmai17 <116304133+kiranmai17@users.noreply.github.com>
2022-10-27 21:40:45 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,21 @@
+'''
+DATE=27th oct 2022
+DAY= thursday
+TOPIC=variables 
+AUTHOR= kiranmai
+'''
+a=10
+_b=20
+A=60
+c_4f=50
+z5=100
+print(a,_b,A,c_4f,z5)   #10 20 60 50 100
+print('pooja'.isidentifier()) #true
+print('3kiranmai'.isidentifier()) #false
+print('_kiranmai'.isidentifier()) #true
+print('hello kiran\'mai') #hello kiran'mai
+print("hello kiran'mai") #hello kiran'mai
+print('kiranstella '*5)
+print('''abcdefghijklmnopqrstuvwxrkirnlpppppppppppppponubdqevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvmkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+hhhhhhhhhhhhh ''')
+
